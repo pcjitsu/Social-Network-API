57480af5339df870581071df3e4b6dede89ce4bb
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-10-26 22:03:25 -0500

    thoguhtController udpate

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -45,20 +45,59 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // async deleteThought(req, res) {
-  //   try {
-  //     const thought = await Thought.findOneAndDelete(
-  //       { _id: req.params.userId })
-        
-  //       if (!thought){
-  //           res.status(404).json({ message: 'No Thought with that ID' })
-  //       }
-        
-  //       ;
-  //   } catch (error) {
-  //       console.log(err)
-  //   }
-  // }
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndDelete({
+        _id: req.params.thoughtId,
+      });
+
+      if (!thought) {
+        res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      res.json({ message: "Thought deleted!" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async createReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.params.reactionId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: "No thought found with that ID :(" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async deleteReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: "No thought found with that ID :(" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
 
 
