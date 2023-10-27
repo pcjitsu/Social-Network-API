d9f193b8107fd0373dff0ac50a7aeb4f8fec5a41
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-10-26 22:11:29 -0500

    finished friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,12 +97,3 @@ module.exports = {
 };
 
 
-
-
-
-
-// /api/users/:userId/friends/:friendId
-
-// POST to add a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
